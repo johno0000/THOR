ab6e6c46e5de7c4be13339da5615fa6ae454b156
johno0000 <110921014+johno0000@users.noreply.github.com>
2022-08-11 18:28:53 -0700

    Add files via upload

--- DataReaderFinal.py
+++ DataReaderFinal.py
@@ -25,11 +25,17 @@ def stringToTable(lmString, dateTime):
     jsonDict = json.loads(re.sub("eRC[0-9]{4} ", "", lmString))['lm_data']
     data = pd.DataFrame.from_dict(jsonDict)
     data['PPS'] = pd.Series([int('{0:08b}'.format(x)[-8]) for x in data['flags']]) ## very useful column for future operations - separates out GPS signal from the Flags column
+    data['Seconds'] = getSecondsFromWallClock(data)
+    data['UnixTime'] = dateTime.timestamp() + data['Seconds'] - data['Seconds'].iloc[-1] ## this assigns dateTime to the final photon in the table, and works backwards from there to previous photons
+    unixTimeCorrection = data[data['PPS'] == 1]['UnixTime'].apply(lambda x: (x - round(x)).median())
+    pulseData = data[data['PPS'] == 1]
+    unixTimeCorrection = (round(pulseData['UnixTime']) - pulseData['UnixTime']).median()
+    data['UnixTime'] = data['UnixTime'] + unixTimeCorrection
     return(data)
 
 def getSecondsFromWallClock(data):
     rolloverCorrection = (data['wc'].diff() < 0).cumsum() * pow(2, 36)  ## Every time you encounter the wallclock going backwards, assume it's rollover. Count how many times its rolled over and multiply that by 2^36 for each row's correction.
-    data['wc'] = data['wc'] + rolloverCorrection
+    data['wc'] = data['wc'] + rolloverCorrection ## Changing the contents of the object passed in the function - there might be a better way to do this but it should work since Python passes by object reference
     wallClockCorrection = (data['wc'][data['PPS'] == 1]).diff().median()
     if(abs(wallClockCorrection - 8e7) > 1e4):
         raise Exception("wallclock and GPS clocks in significant disagreement")
