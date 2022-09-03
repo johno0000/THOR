c4720579ef0503a7435bf0bbae62e0bbdceb3a8a
johno0000 <110921014+johno0000@users.noreply.github.com>
2022-09-03 16:31:28 -0700

    Update DataReaderFinal.py

--- DataReaderFinal.py
+++ DataReaderFinal.py
@@ -25,20 +25,20 @@ def thorFileToData(lines):
         lmString = lines[i]
         timeString = lines[i - 2]
         dateTime = datetime.strptime(timeString, "%Y %m %d %H %M %S %f")
-        data = getDataFromLMthor(lmString, dateTime))
-        data = processDataTiming(data)
+        data = getDataFromLMthor(lmString))
+        data = processDataTiming(data, dateTime)
         dataList.append(data)
     data = pd.concat(dataList)
     return(data)
 
 
-def getDataFromLMthor(lmString, dateTime):
+def getDataFromLMthor(lmString):
     jsonDict = json.loads(re.sub("eRC[0-9]{4} ", "", lmString))['lm_data']
     data = pd.DataFrame.from_dict(jsonDict)
     data['PPS'] = pd.Series([int('{0:08b}'.format(x)[-8]) for x in data['flags']]) ## very useful column for future operations - separates out GPS signal from the Flags column
     return(data)
 
-def processDataTiming(data):
+def processDataTiming(data, dateTime):
     data['Seconds'] = getSecondsFromWallClock(data)
     data['UnixTime'] = dateTime.timestamp() + data['Seconds'] - data['Seconds'].iloc[-1] ## this assigns dateTime to the final photon in the table, and works backwards from there to previous photons
     firstDT = datetime.fromtimestamp(data['UnixTime'].min())
