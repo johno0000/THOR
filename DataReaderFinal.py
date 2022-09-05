3c48e368d06b972e206ef87630a82ea6d4b8ee76
johno0000 <110921014+johno0000@users.noreply.github.com>
2022-09-05 13:31:28 -0700

    Add files via upload

--- DataReaderFinal.py
+++ DataReaderFinal.py
@@ -9,16 +9,15 @@ from scipy.stats import poisson
 from datetime import datetime
 import gzip
 
+
 def fileNameToData(fileName):
     if(fileName[-2:] == 'gz'):
         f = gzip.open(fileName, 'rt')
     else:
         f = open(fileName)
     lines = f.read().splitlines()
-
-    if "xtr" in fileName:
+    if ("xtr" in fileName):
         return(0)
-
     if(len(lines[0]) == 3):
         data = thorFileToData(lines)
     else:
@@ -41,11 +40,26 @@ def thorFileToData(lines):
     data = pd.concat(dataList)
     return(data)
 
+
 def lmFileToData(lines, mode):
     dataList = list()
-    return(datalist)
-
-    
+    if mode is 1:
+        start = 2
+        increment = 2
+        tStamp = 1
+    elif mode is 2:
+        start = 7
+        increment = 6
+        tStamp = 2
+    for i in range(start, len(lines), increment):
+        lmString = lines[i]
+        timeString = lines[i - tStamp]
+        dateTime = datetime.strptime(timeString, "%Y %m %d %H %M %S %f")
+        data = getDataFromLM(lmString, mode)
+        data = processDataTiming(data, dateTime)
+        dataList.append(data)
+    data = pd.concat(dataList)
+    return(data)
 
 
 def getDataFromLMthor(lmString):
@@ -54,14 +68,17 @@ def getDataFromLMthor(lmString):
     data['PPS'] = pd.Series([int('{0:08b}'.format(x)[-8]) for x in data['flags']]) ## very useful column for future operations - separates out GPS signal from the Flags column
     return(data)
 
+
 def processDataTiming(data, dateTime):
     data['Seconds'] = getSecondsFromWallClock(data)
     data['UnixTime'] = dateTime.timestamp() + data['Seconds'] - data['Seconds'].iloc[-1] ## this assigns dateTime to the final photon in the table, and works backwards from there to previous photons
     firstDT = datetime.fromtimestamp(data['UnixTime'].min())
     data['SecondsOfDay'] = data['UnixTime'] - datetime(firstDT.year, firstDT.month, firstDT.day).timestamp() ## subtracts out 00:00:00 of the earliest day in UNIX time. For files that cross midnight this will result in SecondsOfDay exceeding 86000 or whatever
-    unixTimeCorrection = data[data['PPS'] == 1]['SecondsOfDay'].apply(lambda x: (x - round(x))).median()
-    data['SecondsOfDay'] = data['SecondsOfDay'] + unixTimeCorrection
-    data['UnixTime'] = data['UnixTime'] + unixTimeCorrection ## some of this precision is going to get truncated. If that's a big deal then you need to recreate a DateTime object by adding the adjusted SecondsOfDay to firstDT.
+    if(data['gpsSync'].all()):
+        unixTimeCorrection = data[data['PPS'] == 1]['SecondsOfDay'].apply(lambda x: (x - round(x))).median()
+        data['SecondsOfDay'] = data['SecondsOfDay'] + unixTimeCorrection
+        data['UnixTime'] = data['UnixTime'] + unixTimeCorrection ## some of this precision is going to get truncated. If that's a big deal then you need to recreate a DateTime object by adding the adjusted SecondsOfDay to firstDT.
+    data = data.drop('Seconds', axis = 1)
     return(data)
 
 
@@ -71,35 +88,40 @@ def getSecondsFromWallClock(data):
     wallClockCorrection = (data['wc'][data['PPS'] == 1]).diff().median()
     if(abs(wallClockCorrection - 8e7) > 1e4):
         raise Exception("wallclock and GPS clocks in significant disagreement")
+    if(np.isnan(wallClockCorrection)):
+        wallClockCorrection = 8e8
+        data['gpsSync'] = False
+    else:
+        data['gpsSync'] = True
     Seconds = data['wc'] / wallClockCorrection
     return(Seconds)
 
 
-def getDataFromLM(lines, mode):
+def getDataFromLM(lmString, mode):
     if mode is 1:
         start = 2
         rowLength = 3
     elif mode is 2:
         start = 9
         rowLength = 6
-    buffers = [s for s in lines if len(s) > 1000]
-    buffers = [x.split(" ")[start:len(x)] for x in buffers]
-    buffers = [[int(y) for y in x] for x in buffers]
-    data = np.array(buffers, dtype = object)
-    data = [np.reshape(x, (int(len(x)/rowLength), rowLength)) for x in data]
-    data = [pd.DataFrame(x) for x in data]
-    data = pd.concat(data, ignore_index = True)
+    lmStrings = lmString.split(" ")[start:]
+    buffer = [int(y) for y in lmStrings]
+    data = np.reshape(buffer, (int(len(buffer)/rowLength), rowLength))
+    data = pd.DataFrame(data)
+    #data = pd.concat(data, ignore_index = True)
     data = data[~data.duplicated()]
     coarsetick = 65536
     if mode is 2:
         wc = (data[2] + data[3] * coarsetick + data[4] * coarsetick * coarsetick) 
         energy = data[1]
         ticks = pd.Series([int('{0:08b}'.format(x)[-8]) for x in data[5]])
+        flags = data[5]
     elif mode is 1:
         wc = (data[1] + data[2] * coarsetick) 
         energy = data[0]
         ticks = data[0] * 0 ## For some reason makes the pd.concat phase much faster
-    newData = pd.concat([energy.rename('energy'), wc.rename('wc'), ticks.rename('PPS')], axis = 1)
+        flags = data[0] * 0
+    newData = pd.concat([energy.rename('energy'), wc.rename('wc'), ticks.rename('PPS'), flags.rename('flags')], axis = 1)
     return(newData)
 
 
